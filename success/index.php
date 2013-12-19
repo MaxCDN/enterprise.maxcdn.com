edca621058bbe6683a3284c9f62447be9c6b0cea
tjasko <taylor@taylorjasko.com>
2013-12-18 17:44:01 -0800

    removing trusted clients on success

--- success/index.php
+++ success/index.php
@@ -10,26 +10,6 @@
 			</div>
 		</div>
 	</div><!-- /block -->
-	<div class="clients">
-		<span class="str">CURRENT CLIENTS</span>
-		<ul>
-			<li>
-				<span><div class="image kixeye"></div></span>
-			</li>
-			<li>
-				<span><div class="image thenextweb"></div></span>
-			</li>
-			<li>
-				<span><div class="image stumbleupon"></div></span>
-			</li>
-			<li>
-				<span><div class="image jquery"></div></span>
-			</li>
-			<li>
-				<span><div class="image nissan"></div></span>
-			</li>
-		</ul>
-	</div><!-- /clients -->
 </div><!-- /top-area -->
 
 <?php include('../footer.php'); ?>

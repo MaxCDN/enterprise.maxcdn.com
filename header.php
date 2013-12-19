dd561fcc545cb081194a9453e7c9c6923d75bb7a
tjasko <taylor@taylorjasko.com>
2013-12-19 12:17:00 -0800

    etagging support for php

--- header.php
+++ header.php
@@ -1,3 +1,19 @@
+<?php
+// etag support
+// lame, but works... haha
+$lastModified=filemtime(__FILE__);
+$etagFile = md5_file(__FILE__);
+$ifModifiedSince=(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : false);
+$etagHeader=(isset($_SERVER['HTTP_IF_NONE_MATCH']) ? trim($_SERVER['HTTP_IF_NONE_MATCH']) : false);
+header("Last-Modified: ".gmdate("D, d M Y H:i:s", $lastModified)." GMT");
+header("Etag: $etagFile");
+header('Cache-Control: public');
+if (@strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE'])==$lastModified || $etagHeader == $etagFile) {
+       header("HTTP/1.1 304 Not Modified");
+       exit;
+}
+?>
+
 <?php include('functions.php'); ?>
 
 <?php

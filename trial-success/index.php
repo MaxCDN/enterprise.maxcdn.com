9f8e5dc52e3d746542779ea00b533d18bc6ef684
weaverhe <heather@hweaver.com>
2014-02-14 16:35:17 -0800

    LP Updates
    Small form fix, popup/copy changes

--- trial-success/index.php
+++ trial-success/index.php
@@ -6,8 +6,8 @@
 		<div class="tbl-cell">
 			<div class="tbl-in">
 				<strong class="ttl">Thank You!</strong>
-				<p>We’ve received your request for the enterprise free trial. A solutions architect will be in contact with you soon to determine your needs and get you setup. Call us to get started right away at <span class="replace-number">+1 (877) 629-2361</span></p>
-				<p class="trial-success-disclaimer">*To qualify for an enterprise free trial there is a 12 month commitment and a $800/month minimum. Alternatively we have plans starting as low as $9/month with a 30 day money back guarantee.</p>
+				<p>We’ve received your request for the enterprise free trial. A solutions architect will be in contact with you soon to determine your needs and get you setup. You can call us to get started right away at <span class="replace-number">+1 (877) 629-2361</span></p>
+				<p class="trial-success-disclaimer">*To qualify for an enterprise free trial there is a 12month commitment and a $800/month minimum. Alternatively we have plans starting as low as $9/month with a 30 day money back guarantee.</p>
 			</div>
 		</div>
 	</div><!-- /block -->

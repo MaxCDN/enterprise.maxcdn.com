96f0aec19d922b47ea166ecfa9ccdab691385a8b
weaverhe <heather@hweaver.com>
2014-05-28 15:46:53 -0700

    Google Analytics Updates
    Moved GA to the footer and added the updated code

--- minheader.php
+++ minheader.php
@@ -58,15 +58,6 @@ $filePrepender = (substr_count($_SERVER['SCRIPT_NAME'], '/') > 1) ? "../" : "";
 		<link rel="stylesheet" type="text/css" href="css/ie.css" media="all">
 	<![endif]-->
 
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	  ga('create', 'UA-11020602-7', 'maxcdn.com');
-	  ga('send', 'pageview');
-	</script>
-
 	<script type="text/javascript">var _kmq = _kmq || [];
 		  var _kmk = _kmk || '8521c9125b22f8dc648d1abbb95e84667d89841e';
 		  function _kms(u){
